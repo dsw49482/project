8907751e15406775b86d868f6f5065ee3c9b6c3b
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 06:10:43 +0200

    Delete task5.py

--- task5.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-try:
-    with open(output_file_path, 'w') as file:
-        yaml.dump(data, file)
-        print(f'Dane zostały zapisane do pliku {output_file_path}.')
-except FileNotFoundError:
-    print(f'Nie można zapisać danych do pliku {output_file_path}.')
