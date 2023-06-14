42fd60b7f88a62b122ea457a9d4bc809996c71b8
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 06:06:36 +0200

    Delete task3.py

--- task3.py
+++
@@ -1,8 +0,0 @@
-import json
-
-try:
-    with open(output_file_path, 'w') as file:
-        json.dump(data, file, indent=4)
-        print(f'Dane zostały zapisane do pliku {output_file_path}.')
-except FileNotFoundError:
-    print(f'Nie można zapisać danych do pliku {output_file_path}.')
