e22cab0cc6d2d889603273a309e03c35ccca5bfc
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 06:13:42 +0200

    Delete task7.py

--- task7.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-
-try:
-    tree = ET.ElementTree(root)  # root to wcześniej wczytany element
-    tree.write(output_file_path)
-    print(f'Dane zostały zapisane do pliku {output_file_path}.')
-except FileNotFoundError:
-    print(f'Nie można zapisać danych do pliku {output_file_path}.')
