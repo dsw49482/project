21c5cc0f9872cec1ccad8ae792dc45f3f62a1c92
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 06:04:26 +0200

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Program do konwersji danych')
-parser.add_argument('input_file', help='Ścieżka pliku wejściowego')
-parser.add_argument('output_file', help='Ścieżka pliku wyjściowego')
-args = parser.parse_args()
-
-input_file_path = args.input_file
-output_file_path = args.output_file
