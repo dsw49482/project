8601282d34f3d01a7c8fe3e75592f3aede52b603
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 12:33:27 +0200

    Update program.py

--- program.py
+++ program.py
@@ -3,91 +3,63 @@ import json
 import yaml
 import xml.etree.ElementTree as ET
 
-
 def parse_json(file_path):
     with open(file_path, 'r') as file:
-        try:
-            data = json.load(file)
-        except json.JSONDecodeError as e:
-            print("Error parsing JSON file:", str(e))
-            return None
+        data = json.load(file)
     return data
-
 
 def save_json(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-
 def parse_yaml(file_path):
     with open(file_path, 'r') as file:
-        try:
-            data = yaml.safe_load(file)
-        except yaml.YAMLError as e:
-            print("Error parsing YAML file:", str(e))
-            return None
+        data = yaml.safe_load(file)
     return data
-
 
 def save_yaml(file_path, data):
     with open(file_path, 'w') as file:
         yaml.dump(data, file, default_flow_style=False)
 
-
 def parse_xml(file_path):
-    try:
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        return root
-    except ET.ParseError as e:
-        print("Error parsing XML file:", str(e))
-        return None
-
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    return root
 
 def save_xml(file_path, root):
     tree = ET.ElementTree(root)
     with open(file_path, 'wb') as file:
         tree.write(file, encoding='utf-8', xml_declaration=True)
 
-
-def read_text_file(file_path):
+def read_json_file(file_path):
     try:
         with open(file_path, 'r') as file:
-            text = file.read()
-        return text
+            data = json.load(file)
+        return data
     except FileNotFoundError:
         print("Error: File not found:", file_path)
         return None
-
-
-def save_text_file(file_path, text):
-    with open(file_path, 'w') as file:
-        file.write(text)
-
+    except json.JSONDecodeError as e:
+        print("Error parsing JSON file:", str(e))
+        return None
 
 def convert_data(input_file_path, output_file_path):
     file_extension = input_file_path.split('.')[-1].lower()
 
     if file_extension == 'json':
         data = parse_json(input_file_path)
-        if data is not None:
-            save_json(output_file_path, data)
+        save_json(output_file_path, data)
     elif file_extension in ['yml', 'yaml']:
         data = parse_yaml(input_file_path)
-        if data is not None:
-            save_yaml(output_file_path, data)
+        save_yaml(output_file_path, data)
     elif file_extension == 'xml':
         root = parse_xml(input_file_path)
-        if root is not None:
-            save_xml(output_file_path, root)
+        save_xml(output_file_path, root)
     else:
-        # Treat as plain text file
-        text = read_text_file(input_file_path)
-        if text is not None:
-            save_text_file(output_file_path, text)
+        print(f"Unsupported file format: {file_extension}")
+        return
 
     print(f"Data converted successfully. Saved to {output_file_path}")
-
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -95,4 +67,10 @@ if __name__ == '__main__':
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
+        
+        json_data = read_json_file(input_file)
+        if json_data is not None:
+            print("JSON data:")
+            print(json_data)
+        
         convert_data(input_file, output_file)
